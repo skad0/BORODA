6f9e668f1e601ce7dc5e174f396dc18e42ea46f4
Anton Krichevskii <mrskadi0@gmail.com>
2016-08-30 22:27:14 +0300

    Changed static rendering, angular (other libs) provided by bower

--- app/server.js
+++ app/server.js
@@ -1,17 +1,32 @@
 'use strict';
 
-const path  = require('path');
 const koa   = require('koa');
 const route = require('koa-route');
-const serve = require('koa-static');
+const send  = require('koa-send');
 
-const app   = koa();
+const app = koa();
+
+app.use(function *(next) {
+    // go to next middleware if it about api
+    if (this.path.substr(0, 4).toLowerCase() === '/api') {
+        yield next;
+        return;
+    }
+    // file exists and its ok
+    if (yield send(this, this.path, { root: 'app/view' })) {
+        return;
+    }
+    if (this.path.indexOf('.') !== -1) {
+      // file does not exist so do nothing and koa will return 404 by default
+      // we treat any path with a dot '.' in it as a request for a file
+      return;
+    }
+    yield send(this, '/index.html', { root: 'app/view' });
+});
 
 app.use(route.get('/api/vote', function *() {
     this.body = 'vote';
 }));
-
-app.use(serve(path.join(`${__dirname}/view/`)));
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
